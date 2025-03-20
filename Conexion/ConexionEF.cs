59521d5a04d7f68596c4bab754c8fee96aa5a659
licopx <licopx11@gmail.com>
2025-03-19 20:42:31 -0500

    feat(conexión) Se conectaron las últimas clases con la base de datos y, se agregó la funcionalidad de insertar datos desde c# a la bd.

--- Conexion/ConexionEF.cs
+++ Conexion/ConexionEF.cs
@@ -24,8 +24,12 @@ namespace Hotel.Conexion{
         var lista_opiniones = conexion.Opiniones!.ToList();
         var lista_clientes = conexion.Clientes!.ToList();
         var lista_reservas = conexion.Reservas!.ToList();
+        var lista_servicios = conexion.Servicios!.ToList();
+        var lista_servicios_reservas = conexion.Servicios_Reservas!.ToList();
+        var lista_promociones = conexion.Promociones!.ToList();
+        var lista_pagos = conexion.Pagos!.ToList();
 
-        Console.WriteLine("Nombre | Camas | Estado");
+        /*Console.WriteLine("Nombre | Camas | Estado");
         foreach(var habitacion in lista_habitaciones){
 
             Console.WriteLine($"{habitacion.Nombre}    |  {habitacion.Camas} |     {habitacion.Estado}");
@@ -66,6 +70,67 @@ namespace Hotel.Conexion{
 
             Console.WriteLine($"{reserva.Id}  |  {reserva.Codigo} |  {reserva.Fecha_Entrada}  |  {reserva.Cliente} |  {reserva.Recepcionista}  |  {reserva.Habitacion}");
 
+        }*/
+
+    }
+
+        public void ConexionInsert(){
+
+            var conexion = new Conexion();
+
+            conexion.StringConnection = this.string_connection;
+
+            var servicios_reservas = new Servicios_Reservas();
+
+            var servicio = conexion.Servicios!.FirstOrDefault(x => x.Tipo == "Piscina");
+            var reserva = conexion.Reservas!.FirstOrDefault(x => x.Id == 1);
+
+            servicios_reservas.Servicio = servicio!.Id;
+            servicios_reservas.Reserva = reserva!.Id;
+            conexion.Servicios_Reservas!.Add(servicios_reservas);
+            conexion.SaveChanges();
+
+            /*servicio = conexion.Servicios!.FirstOrDefault(x => x.Tipo == "Restaurante");
+            reserva = conexion.Reservas!.FirstOrDefault(x => x.Id == 1);
+            servicios_reservas.Servicio = servicio!.Id;
+            servicios_reservas.Reserva = reserva!.Id;
+            conexion.Servicios_Reservas!.Add(servicios_reservas);
+            conexion.SaveChanges();
+
+            servicio = conexion.Servicios!.FirstOrDefault(x => x.Tipo == "Limpieza");
+            reserva = conexion.Reservas!.FirstOrDefault(x => x.Id == 2);
+            servicios_reservas.Servicio = servicio!.Id;
+            servicios_reservas.Reserva = reserva!.Id;
+            conexion.Servicios_Reservas.Add(servicios_reservas);
+            conexion.SaveChanges();
+
+            servicio = conexion.Servicios!.FirstOrDefault(x => x.Tipo == "Discoteca");
+            reserva = conexion.Reservas!.FirstOrDefault(x => x.Id == 3);
+            servicios_reservas.Servicio = servicio!.Id;
+            servicios_reservas.Reserva = reserva!.Id;
+            conexion.Servicios_Reservas.Add(servicios_reservas);
+            conexion.SaveChanges();*/
+        }
+        public void ConexionCliente(){
+
+            var conexion = new Conexion();
+            conexion.StringConnection = this.string_connection;
+
+            var cliente = new Clientes();
+
+            Console.WriteLine("---  Proceso de registro de cliente  ---");
+
+            Console.WriteLine("Ingrese su cédula: \n-> ");
+            cliente.Cedula = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su nombre: \n-> ");
+            cliente.Nombre = Console.ReadLine();
+
+            var opinion = conexion.Opiniones!.FirstOrDefault(x => x.Opcion == "Mala");
+            cliente.Opinion = opinion!.Id;
+            
+            conexion.Clientes!.Add(cliente);
+            conexion.SaveChanges();
         }
 
     }
@@ -89,9 +154,11 @@ namespace Hotel.Conexion{
         public DbSet<Opiniones>? Opiniones {get;set;}
         public DbSet<Clientes>? Clientes {get;set;}
         public DbSet<Reservas>? Reservas {get;set;}
+        public DbSet<Servicios>? Servicios {get;set;}
+        public DbSet<Servicios_Reservas>? Servicios_Reservas {get;set;}
+        public DbSet<Promociones>? Promociones {get;set;}
+        public DbSet<Pagos>? Pagos {get;set;}
 
     }
-
-}
 
 }
